24a43698bcc11eae1e30cfc1e81740e0864f3c3c
Ondřej Staněk <viego443@proton.me>
2024-08-21 14:33:04 +0200

    updated deps, fixed a readme typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <h1 id="main-title" class="mt-32 mb-16 text-center">
 	Chceš se stát <b><u id="champ">šampionem</u></b>?
 </h1>
-<p>Českolipská Esports je šancí, díky které se šampionem může stát kdokoliv.</p>
-<div class="mx-auto"></div>
 
 <style lang="scss">
 	#main-title {
@@ -15,7 +13,7 @@
 	}
 
 	#champ {
-		color: white;
+		color: white;               
 		font-size: 5rem;
 		text-decoration: underline var(--accent-color) 0.4rem;
 		text-shadow: 0px 0px 5px white;
