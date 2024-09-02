2befcb72a0cb528d6d71b34c2643941c14c07899
Ondřej Staněk <viego443@proton.me>
2024-09-02 17:23:47 +0200

    finally got rid of the old supabase client

--- src/lib/supabaseClient.ts
+++
@@ -1,5 +0,0 @@
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
-import { createClient } from '@supabase/supabase-js';
-import type { Database } from '../database.types';
-
-export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
