43222c8741a1e18de56053c38430965915696046
Ondřej Staněk <viego443@proton.me>
2024-09-03 20:57:19 +0200

    installed tailwind class sorter plugin, added some more global variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,20 @@
 import type { Config } from 'tailwindcss';
+import typography from '@tailwindcss/typography';
 
 export default {
 	content: ['./src/**/*.{js,ts,html,svelte}'],
 	theme: {
 		extend: {
 			colors: {
-				accent: '#ee0000'
+				accent: '#ee0000',
+				'less-important': 'rgb(190, 190, 190)',
+				'text-color': 'rgb(240, 240, 240)',
+				outline: 'rgb(70, 70, 70)',
+				background: {
+					button: 'rgb(25, 25, 25)'
+				}
 			}
 		}
 	},
-	plugins: []
+	plugins: [typography]
 } satisfies Config;
