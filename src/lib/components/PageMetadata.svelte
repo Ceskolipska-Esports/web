4a858d61cc407bd57b37205748673f717b069788
Ondřej Staněk <viego443@proton.me>
2024-09-17 15:28:06 +0200

    fixed homepage button, added ogp

---
+++ src/lib/components/PageMetadata.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let title: string = "Českolipská Esports";
+	export let type: string;
+	export let url: string;
+	export let image: string = "icons/favicon.png";
+	export let locale: string = "cs_CZ";
+	export let description: string | undefined = undefined;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta property="og:title" content={title} />
+	<meta property="og:site_name" content="Českolipská Esports" />
+	<meta property="og:type" content={type} />
+	<meta property="og:url" content={url} />
+	<meta property="og:image" content={image} />
+	<meta property="og:locale" content={locale} />
+
+	{#if description}
+	<meta property="og:description" content={description} />
+	{/if}
+</svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import Link from '$lib/components/Link.svelte';
-	import PageMetadataHelper from '$lib/components/PageMetadataHelper.svelte';
+	import PageMetadata from "$lib/components/PageMetadata.svelte";
 
 	import globals from '$lib/globals';
 </script>
 
-<PageMetadataHelper description="E-sportová organizace Českolipská Esports" />
+<PageMetadata url="https://ceskolipskaesports.cz" type="website" />
+
 <h1 class="my-32 text-center text-5xl text-less-important sm:text-7xl md:text-8xl">
 	Chceš se stát <b
 		><u class="text-white underline decoration-accent decoration-4 drop-shadow-[0_0_3px_white]"
@@ -22,6 +23,6 @@
 	</p>
 	<a
 		class="mx-auto flex w-fit items-center gap-2 rounded-2xl border-2 border-outline bg-background-button p-4 text-2xl text-white no-underline transition-all duration-150 hover:scale-105 hover:border-accent hover:drop-shadow-[0_0_0.5rem_theme(colors.accent)]"
-		href={globals.discordInvite}>Discord server <i class="bx bx-chevron-right text-2xl" /></a
+	    target="_blank" href={globals.discordInvite}>Discord server <i class="bx bx-chevron-right text-2xl" /></a
 	>
 </div>
